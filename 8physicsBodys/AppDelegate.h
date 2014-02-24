9a28b9e1aa7a9dd343fb727f187f327de562e75b
Nan Li <linan1114@gmail.com>
2014-02-24 11:50:28 -0500

    Initial Commit

---
+++ 8physicsBodys/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  8physicsBodys
+//
+//  Created by linan on 2/24/14.
+//  Copyright (c) 2014 NanLi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 8physicsBodys/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  8physicsBodys
+//
+
+//  Copyright (c) 2014 NanLi. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
